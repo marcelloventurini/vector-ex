862ecdb8f26e607ffd7e7c1a7516e861a9fc3395
Marcello Venturini <75899197+marcelloventurini@users.noreply.github.com>
2023-05-19 16:27:55 -0300

    Add files via upload

---
+++ VectEx/Program.cs
@@ -0,0 +1,31 @@
+﻿using VectEx;
+
+Rent[] rent = new Rent[10];
+
+Console.Write("Quantos quartos serão alugados? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Aluguel {i + 1}:");
+    Console.Write("Nome: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Email: ");
+    string email = Console.ReadLine();
+
+    Console.Write("Quarto: ");
+    int room = int.Parse(Console.ReadLine());
+
+    rent[i] = new Rent { Name = name, Email = email, Room = room };
+}
+
+Console.WriteLine("Quartos ocupados: ");
+
+for (int i = 0; i < rent.Length; i++)
+{
+    if (rent[i] != null)
+    {
+        Console.WriteLine(rent[i]);
+    }
+}
---
+++ VectEx/Rent.cs
@@ -0,0 +1,14 @@
+﻿namespace VectEx
+{
+    internal class Rent
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Room}: {Name}, {Email}";
+        }
+    }
+}
